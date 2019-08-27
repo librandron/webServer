25356f7d46f48f775c55546f4580f32574d0f813
Андрей М <librandron@gmail.com>
2019-08-27 21:07:44 +0300

    some json reader

--- webServer/Program.cs
+++ webServer/Program.cs
@@ -7,6 +7,7 @@ using System.Threading.Tasks;
 using System.IO;
 using Newtonsoft.Json;
 using webServer.Domain;
+using System.Web;
 
 namespace webServer
 {
@@ -35,19 +36,8 @@ namespace webServer
                     byte[] fileAsByte;
                     Stream output;
 
-                    if(request.Url.LocalPath.Contains("part.html"))
-                    {
-                        file = File.ReadAllText(requestPathFile);
-                        var fileJson = File.ReadAllLines(@"D:\");
-                        Database data = JsonConvert.DeserializeObject<Database>(fileJson);
-
-
-                        fileAsByte = System.Text.Encoding.UTF8.GetBytes(file);
-                        response.ContentLength64 = fileAsByte.Length;
-                        output = response.OutputStream;
-                        output.Write(fileAsByte, 0, fileAsByte.Length);
-                        output.Close();
-                    }
+                    if (request.Url.LocalPath.Contains("part.html"))
+                        HandleParticipantsRoute(response, requestPathFile, out file, out fileAsByte, out output);
 
                 }
 
@@ -57,12 +47,45 @@ namespace webServer
                     fileAsByte = System.Text.Encoding.UTF8.GetBytes(file);
                     response.ContentLength64 = fileAsByte.Length;
                     var output = response.OutputStream;
-                    output.Write(fileAsByte, 0, fileAsByte.Length);
-                    output.Close();
+                    var output.Write(fileAsByte, 0, fileAsByte.Length);
+                    var output.Close();
                 }
 
 
             }
+
+        private static void HandleParticipantsRoute(HttpListenerResponse response, string requestPathFile, out string file, out byte[] fileAsByte, out Stream output)
+        {
+            file = File.ReadAllText(requestPathFile);
+            var fileJson = File.ReadAllLines(@"D:\");
+            //Database data = JsonConvert.DeserializeObject<Database>(fileJson);
+
+
+            fileAsByte = System.Text.Encoding.UTF8.GetBytes(file);
+            response.ContentLength64 = fileAsByte.Length;
+            output = response.OutputStream;
+            output.Write(fileAsByte, 0, fileAsByte.Length);
+            output.Close();
         }
+
+
+        public static void HandleVoteRouter(string queryString)
+        {
+            var params2 = HttpUtility.ParseQueryString(queryString);
+            if(params2["attend"]=="true")
+            {
+                var name = params2["name"];
+                var fileJson = File.ReadAllText();
+                Database data = JsonConvert.DeserializeObject<Database>(fileJson);
+                data.users.Add(new Users() { name = name});//add to JSON
+
+                File.WriteAllText(JsonConvert.SerializeObject(data));
+
+            }
+        }
+    }
+
+
+
     }
 }
