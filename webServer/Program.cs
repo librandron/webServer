85a61b9f3ecc3db3d8dae175de12a800f6d4a4c8
Андрей М <librandron@gmail.com>
2019-08-29 21:10:30 +0300

    add some unknown :(

--- webServer/Program.cs
+++ webServer/Program.cs
@@ -8,6 +8,7 @@ using System.IO;
 using Newtonsoft.Json;
 using webServer.Domain;
 using System.Web;
+using webServer.Controller;
 
 namespace webServer
 {
@@ -23,71 +24,28 @@ namespace webServer
 
             listener.Prefixes.Add("http://*:8881/");
             listener.Start();
-            while(listener.IsListening)
-                {
+            while (listener.IsListening)
+            {
                 var pathIndex = @"D:\";
                 HttpListenerContext context = listener.GetContext();
                 HttpListenerRequest request = context.Request;
-                HttpListenerResponse response = context.Response;
+                BaseController targetController;
 
-
-
-               
-
-                if(File.Exists(requestPathFile))
+                if (request.Url.LocalPath.Contains("vote"))
                 {
-                    
-
-                    //if (request.Url.LocalPath.Contains("part.html"))
-                       // HandleParticipantsRoute(response, requestPathFile, out file, out fileAsByte, out output);
-
+                    targetController = new VoteController();
                 }
-
-
-                    file = File.ReadAllText(requestPathFile);
-
-                    fileAsByte = System.Text.Encoding.UTF8.GetBytes(file);
-                    response.ContentLength64 = fileAsByte.Length;
-                     output = response.OutputStream;
-                     output.Write(fileAsByte, 0, fileAsByte.Length);
-                     output.Close();
+                else if (request.Url.LocalPath.Contains("part"))
+                {
+                    targetController = new ParticipantsController();
                 }
-
-
-            }
-
-        private static void HandleParticipantsRoute(HttpListenerResponse response, string requestPathFile, out string file, out byte[] fileAsByte, out Stream output)
-        {
-            file = File.ReadAllText(requestPathFile);
-            var fileJson = File.ReadAllLines(@"D:\");
-            //Database data = JsonConvert.DeserializeObject<Database>(fileJson);
-
-
-            fileAsByte = System.Text.Encoding.UTF8.GetBytes(file);
-            response.ContentLength64 = fileAsByte.Length;
-            output = response.OutputStream;
-            output.Write(fileAsByte, 0, fileAsByte.Length);
-            output.Close();
-        }
-
-
-        public static void HandleVoteRouter(string queryString)
-        {
-            var params2 = HttpUtility.ParseQueryString(queryString);
-            if(params2["attend"]=="true")
-            {
-                var name = params2["name"];
-                var fileJson = File.ReadAllText();
-                Database data = JsonConvert.DeserializeObject<Database>(fileJson);
-                data.users.Add(new Users() { name = name});//add to JSON
-
-                File.WriteAllText(JsonConvert.SerializeObject(data));
-
+                else
+                {
+                    targetController = new IndexController();
+                }
+                targetController.Handle(context);
             }
         }
-    }
-
-
 
     }
 }
